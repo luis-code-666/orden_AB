f36e377bb25d8b7ce0f930724775982700c7b9e8
rockband_666 <rockband_666@hotmail.com>
2024-07-20 23:46:26 -0400

    instalkacion de Pillow para que fuuncione bien

--- ordenapp/models.py
+++ ordenapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    photo = models.ImageField(
+        upload_to="logos", null=True, blank=True, verbose_name="foto"
+    )
+
+    def __str__(self):
+        return self.photo
