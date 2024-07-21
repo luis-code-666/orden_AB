bc189aef26f45422880b84cb5e7d85b5867abb4f
rockband_666 <rockband_666@hotmail.com>
2024-07-20 23:27:17 -0400

    creacion de requirrements y requirements-dev

--- orden/urls.py
+++ orden/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('orden/', include("ordenapp.urls")),
 ]
