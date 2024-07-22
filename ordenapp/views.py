264237b09ae13d903c8e181ce1fda4450468264e
rockband_666 <rockband_666@hotmail.com>
2024-07-22 00:33:45 -0400

    terminado pero hay un error con el metodo str y aque eso llama una cadena de texto y no imagen para usar la imagen se esta aberiguando eso

--- ordenapp/views.py
+++ ordenapp/views.py
@@ -1,7 +1,14 @@
 
 from django.views import generic
+from django.urls import reverse_lazy
 from .forms import OrdenDiaForm
 
 class MiOrdenDelDia(generic.FormView):
     template_name = "orden/orden.html"    
     form_class = OrdenDiaForm
+    success_url = reverse_lazy("orden_dia")
+    
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
