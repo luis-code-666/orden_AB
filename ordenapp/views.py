906ae80196ff1f7d13f12f7ee77c9f39933562ad
rockband_666 <rockband_666@hotmail.com>
2024-07-22 00:00:33 -0400

    solucion de error en template_name

--- ordenapp/views.py
+++ ordenapp/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+
 from django.views import generic
 from .forms import OrdenDiaForm
 
 class MiOrdenDelDia(generic.FormView):
-    tempate_name = "orden/orden.html"    
+    template_name = "orden/orden.html"    
     form_class = OrdenDiaForm
