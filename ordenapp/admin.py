819448fc68acbfe74561568f31bc5f8be771f284
rockband_666 <rockband_666@hotmail.com>
2024-07-27 23:36:50 -0400

    corregir la imagen ya que no muestra con estos cambios que se realizo

--- ordenapp/admin.py
+++ ordenapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import OrdenDia
 
-# Register your models here.
+class OrdenDiaAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'photo_tag')
+
+admin.site.register(OrdenDia, OrdenDiaAdmin)
--- ordenapp/models.py
+++ ordenapp/models.py
@@ -1,7 +1,18 @@
+
 from django.db import models
+from django.utils.html import mark_safe
 
 class OrdenDia(models.Model):
     photo = models.ImageField(upload_to="logos", null=True, blank=True, verbose_name="foto")
 
     def __str__(self):
-        return self.photo
+        # Devuelve una cadena de texto, como el nombre del archivo o un identificador
+        return f"Orden del día {self.id}" if self.photo else "Sin Foto"
+
+    def photo_tag(self):
+        if self.photo:
+            return mark_safe(f'<img src="{self.photo.url}" width="350" height="350" />')
+        return "No Image"
+
+    photo_tag.short_description = 'Foto'
+
